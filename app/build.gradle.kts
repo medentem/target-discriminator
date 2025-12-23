e9d6f8872be6c5f4dcd740917c3131f5096a0371
medentem <info@medentem.com>
2025-12-23 12:38:50 -0600

    more media

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 
 android {
     namespace = "com.targetdiscriminator"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.targetdiscriminator"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
